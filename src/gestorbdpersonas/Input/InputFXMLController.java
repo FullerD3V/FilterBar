5528739d57a6ee4171b4ec9f04a701fabde0bec6
FullerD3V <97800761+FullerD3V@users.noreply.github.com>
2022-10-10 17:34:18 +0200

    Barra de búsqueda añadida, sin funcionalidad

---
+++ src/gestorbdpersonas/Input/InputFXMLController.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestorbdpersonas.Input;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import gestorbdpersonas.Person.Persona;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author alumno
+ */
+public class InputFXMLController implements Initializable {
+    
+    @FXML
+    private TextField txtNombre, txtEdad, txtApellidos;
+    
+    @FXML
+    private Button btnGuardar, btnSalir; 
+    
+    String nombre, apellidos;
+    
+    int edad;
+    
+    public static ObservableList<Persona> personas = FXCollections.observableArrayList();
+    
+    @FXML
+    private void txtNombreKeyReleased(KeyEvent event){
+        nombre = txtNombre.getText();
+    }
+    
+    @FXML
+    private void txtNombreKeyTyped(KeyEvent event){
+        if (!event.getCharacter().matches("[A-z]")) {
+            event.consume();
+        }
+    }
+    
+    @FXML
+    private void txtApellidosKeyReleased(KeyEvent event){
+        apellidos = txtApellidos.getText();
+    }
+    
+    @FXML
+    private void txtEdadKeyReleased(KeyEvent event){
+        edad = Integer.valueOf(txtEdad.getText());
+    }
+    
+    @FXML
+    public void txtEdadKeyTyped(KeyEvent event) {
+        if (!event.getCharacter().matches("[0-9]")) {
+            event.consume();
+        }
+    }
+    
+    @FXML
+    public void btnSalirOnAction(){
+    Stage stage = (Stage) btnSalir.getScene().getWindow();
+    stage.close();
+    }
+    
+    @FXML
+    public void btnGuardarOnAction(){    
+        Persona p = new Persona(nombre, apellidos, edad);
+        if(!this.personas.contains(p)){
+            this.personas.add(p);  
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Datos insertados");
+            alert.setHeaderText("Persona añadida correctamente");
+            alert.showAndWait();
+
+            Stage stage = (Stage) btnSalir.getScene().getWindow();
+            stage.close();
+        } else {
+            System.out.println("Ya existe");
+        }
+         
+
+        
+    }
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/gestorbdpersonas/Main/MainFXMLController.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestorbdpersonas.Main;
+
+import gestorbdpersonas.GestorBDPersonas;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
+import gestorbdpersonas.Person.Persona;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import gestorbdpersonas.Input.InputFXMLController;
+
+/**
+ *
+ * @author alumno
+ */
+public class MainFXMLController implements Initializable {
+    
+    @FXML
+    private Button btnAgregar, btnModificar, btnEliminar;
+    
+    @FXML 
+    private TableView tblPersona;
+    
+    @FXML
+    private TableColumn colNombre, colApellidos, colEdad;
+    
+    @FXML
+    private void btnAgregarOnAction(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(gestorbdpersonas.GestorBDPersonas.class.getResource("Input/InputFXML.fxml"));
+
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+        stage.setTitle("Nuevo usuario");
+        stage.setScene(scene);
+        stage.initModality(Modality.WINDOW_MODAL);        
+        stage.initOwner(((Node)event.getSource()).getScene().getWindow());
+        stage.setResizable(false);
+        stage.show();
+        
+        tblPersona.setItems(InputFXMLController.personas);
+    }
+    
+    @FXML
+    private void btnAgregarOnMouseDragOver(){
+        btnAgregar.setStyle("-fx-background-color: AQUAMARINE; -fx-border-color: BLUE");
+    }
+    
+    @FXML 
+    private void btnModificarOnAction(){
+        //De momento nada
+    }
+    
+    @FXML
+    private void btnEliminarOnAction(){
+        //De momento nada
+    }
+    
+    @FXML
+    private void txtBuscarOnKeyReleased(KeyEvent event){}
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        this.colNombre.setCellValueFactory(new PropertyValueFactory("nombre"));
+        this.colApellidos.setCellValueFactory(new PropertyValueFactory("apellidos"));
+        this.colEdad.setCellValueFactory(new PropertyValueFactory("edad"));
+    }        
+}
